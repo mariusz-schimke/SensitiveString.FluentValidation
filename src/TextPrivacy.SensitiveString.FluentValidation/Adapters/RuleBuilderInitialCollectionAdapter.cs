05e12e2c56f5d8a3cf3151d27617aece4add434c
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-03-02 17:32:55 +0100

    Add further implementations

--- src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderInitialCollectionAdapter.cs
+++ src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderInitialCollectionAdapter.cs
@@ -5,41 +5,49 @@ namespace TextPrivacy.SensitiveString.FluentValidation.Adapters;
 
 internal class RuleBuilderInitialCollectionAdapter<TRequest> : IRuleBuilderInitialCollection<TRequest, string>
 {
-    private readonly IRuleBuilderInitialCollection<TRequest, SensitiveString> _originalRule;
+    private readonly IRuleBuilderInitialCollection<TRequest, SensitiveString> _source;
 
-    public RuleBuilderInitialCollectionAdapter(IRuleBuilderInitialCollection<TRequest, SensitiveString> originalRule)
+    public RuleBuilderInitialCollectionAdapter(IRuleBuilderInitialCollection<TRequest, SensitiveString> source)
     {
-        _originalRule = originalRule;
+        _source = source;
     }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator(IPropertyValidator<TRequest, string> validator)
     {
         var validatorAdapter = new PropertyValidatorAdapter<TRequest>(validator);
-        var result = _originalRule.SetValidator(validatorAdapter);
+        var result = _source.SetValidator(validatorAdapter);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
     public IRuleBuilderOptions<TRequest, string> SetAsyncValidator(IAsyncPropertyValidator<TRequest, string> validator)
     {
         var validatorAdapter = new AsyncPropertyValidatorAdapter<TRequest>(validator);
-        var result = _originalRule.SetAsyncValidator(validatorAdapter);
+        var result = _source.SetAsyncValidator(validatorAdapter);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator(IValidator<string> validator, params string[] ruleSets)
     {
         var validatorAdapter = new ValidatorAdapter(validator);
-        var result = _originalRule.SetValidator(validatorAdapter, ruleSets);
+        var result = _source.SetValidator(validatorAdapter, ruleSets);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(
         Func<TRequest, TValidator> validatorProvider, params string[] ruleSets)
-        where TValidator : IValidator<string> =>
-        throw new NotImplementedException();
+        where TValidator : IValidator<string>
+    {
+        var validatorAdapter = ValidatorAdapter.Convert(validatorProvider);
+        var result = _source.SetValidator(validatorAdapter, ruleSets);
+        return new RuleBuilderOptionsAdapter<TRequest>(result);
+    }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(
         Func<TRequest, string, TValidator> validatorProvider, params string[] ruleSets)
-        where TValidator : IValidator<string> =>
-        throw new NotImplementedException();
+        where TValidator : IValidator<string>
+    {
+        var validatorAdapter = ValidatorAdapter.Convert(validatorProvider);
+        var result = _source.SetValidator(validatorAdapter, ruleSets);
+        return new RuleBuilderOptionsAdapter<TRequest>(result);
+    }
 }
--- src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderOptionsAdapter.cs
+++ src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderOptionsAdapter.cs
@@ -5,28 +5,28 @@ namespace TextPrivacy.SensitiveString.FluentValidation.Adapters;
 
 internal class RuleBuilderOptionsAdapter<TRequest> : IRuleBuilderOptions<TRequest, string>
 {
-    private readonly IRuleBuilderOptions<TRequest, SensitiveString> _ruleBuilderOptions;
+    private readonly IRuleBuilderOptions<TRequest, SensitiveString> _source;
 
-    public RuleBuilderOptionsAdapter(IRuleBuilderOptions<TRequest, SensitiveString> ruleBuilderOptions)
+    public RuleBuilderOptionsAdapter(IRuleBuilderOptions<TRequest, SensitiveString> source)
     {
-        _ruleBuilderOptions = ruleBuilderOptions;
+        _source = source;
     }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator(IPropertyValidator<TRequest, string> validator)
     {
-        var result = _ruleBuilderOptions.SetValidator(new PropertyValidatorAdapter<TRequest>(validator));
+        var result = _source.SetValidator(new PropertyValidatorAdapter<TRequest>(validator));
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
     public IRuleBuilderOptions<TRequest, string> SetAsyncValidator(IAsyncPropertyValidator<TRequest, string> validator)
     {
-        var result = _ruleBuilderOptions.SetAsyncValidator(new AsyncPropertyValidatorAdapter<TRequest>(validator));
+        var result = _source.SetAsyncValidator(new AsyncPropertyValidatorAdapter<TRequest>(validator));
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
     public IRuleBuilderOptions<TRequest, string> SetValidator(IValidator<string> validator, params string[] ruleSets)
     {
-        var result = _ruleBuilderOptions.SetValidator(new ValidatorAdapter(validator), ruleSets);
+        var result = _source.SetValidator(new ValidatorAdapter(validator), ruleSets);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
@@ -34,7 +34,7 @@ internal class RuleBuilderOptionsAdapter<TRequest> : IRuleBuilderOptions<TReques
         where TValidator : IValidator<string>
     {
         var validatorAdapter = ValidatorAdapter.Convert(validatorProvider);
-        var result = _ruleBuilderOptions.SetValidator(validatorAdapter, ruleSets);
+        var result = _source.SetValidator(validatorAdapter, ruleSets);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
@@ -42,10 +42,13 @@ internal class RuleBuilderOptionsAdapter<TRequest> : IRuleBuilderOptions<TReques
         where TValidator : IValidator<string>
     {
         var validatorAdapter = ValidatorAdapter.Convert(validatorProvider);
-        var result = _ruleBuilderOptions.SetValidator(validatorAdapter, ruleSets);
+        var result = _source.SetValidator(validatorAdapter, ruleSets);
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
-    public IRuleBuilderOptions<TRequest, string> DependentRules(Action action) =>
-        throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> DependentRules(Action action)
+    {
+        var result = _source.DependentRules(action);
+        return new RuleBuilderOptionsAdapter<TRequest>(result);
+    }
 }

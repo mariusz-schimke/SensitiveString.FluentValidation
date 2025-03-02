9ba68300d2cd48d18531596d1825d339a65efb30
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-03-02 16:56:27 +0100

    Cleanup

--- src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderOptionsAdapter.cs
+++ src/TextPrivacy.SensitiveString.FluentValidation/Adapters/RuleBuilderOptionsAdapter.cs
@@ -18,13 +18,26 @@ internal class RuleBuilderOptionsAdapter<TRequest> : IRuleBuilderOptions<TReques
         return new RuleBuilderOptionsAdapter<TRequest>(result);
     }
 
-    public IRuleBuilderOptions<TRequest, string> SetAsyncValidator(IAsyncPropertyValidator<TRequest, string> validator) => throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> SetAsyncValidator(IAsyncPropertyValidator<TRequest, string> validator)
+    {
+        var result = _ruleBuilderOptions.SetAsyncValidator(new AsyncPropertyValidatorAdapter<TRequest>(validator));
+        return new RuleBuilderOptionsAdapter<TRequest>(result);
+    }
 
-    public IRuleBuilderOptions<TRequest, string> SetValidator(IValidator<string> validator, params string[] ruleSets) => throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> SetValidator(IValidator<string> validator, params string[] ruleSets)
+    {
+        var result = _ruleBuilderOptions.SetValidator(new ValidatorAdapter(validator), ruleSets);
+        return new RuleBuilderOptionsAdapter<TRequest>(result);
+    }
 
-    public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(Func<TRequest, TValidator> validatorProvider, params string[] ruleSets) where TValidator : IValidator<string> => throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(Func<TRequest, TValidator> validatorProvider, params string[] ruleSets)
+        where TValidator : IValidator<string> =>
+        throw new NotImplementedException();
 
-    public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(Func<TRequest, string, TValidator> validatorProvider, params string[] ruleSets) where TValidator : IValidator<string> => throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> SetValidator<TValidator>(Func<TRequest, string, TValidator> validatorProvider, params string[] ruleSets)
+        where TValidator : IValidator<string> =>
+        throw new NotImplementedException();
 
-    public IRuleBuilderOptions<TRequest, string> DependentRules(Action action) => throw new NotImplementedException();
+    public IRuleBuilderOptions<TRequest, string> DependentRules(Action action) =>
+        throw new NotImplementedException();
 }
